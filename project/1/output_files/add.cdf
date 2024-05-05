b308451fb56796fcb73082847ade32e4dfeeb293
wushenhaoyu <1192511920@qq.com>
2024-05-05 16:12:08 +0800

    3

---
+++ project/1/output_files/add.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.0.0 Build 156 04/24/2013 SJ Full Version */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(5CEBA4F23) Path("D:/HuaweiMoveData/Users/SJK/Desktop/1/output_files/") File("add.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ project/1/simulation/qsim/work/@1343_2@040/_primary.vhd
@@ -0,0 +1,16 @@
+library verilog;
+use verilog.vl_types.all;
+entity \3_2\ is
+    port(
+        \0\             : out    vl_logic;
+        \01\            : in     vl_logic;
+        stop            : in     vl_logic;
+        \1\             : out    vl_logic;
+        \2\             : out    vl_logic;
+        \3\             : out    vl_logic;
+        \4\             : out    vl_logic;
+        \5\             : out    vl_logic;
+        \6\             : out    vl_logic;
+        Clk             : in     vl_logic
+    );
+end \3_2\;
